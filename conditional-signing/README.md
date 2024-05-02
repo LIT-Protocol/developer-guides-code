c0dd2ac3ca8e8fd52f174a91dbe58dae19f8fcdc
Wyatt Barnes <me@wyatt.email>
2024-05-01 14:36:33 -1000

    WIP

---
+++ conditional-signing/README.md
@@ -0,0 +1,8 @@
+# Running this Example
+
+1. `yarn`
+2. `yarn start`
+3. Click the `Click Me` button
+4. Connect your wallet
+5. Sign a message to generate an AuthSig
+6. Sign a second message to generate a SessionSig
---
+++ conditional-signing/src/litAction.js
@@ -0,0 +1,26 @@
+export const litActionCode = `
+const go = async () => {
+  // test an access control condition
+  const testResult = await Lit.Actions.checkConditions({conditions, authSig, chain})
+
+  console.log('testResult', testResult)
+
+  // only sign if the access condition is true
+  if (!testResult){
+    return;
+  }
+
+  const message = new Uint8Array(
+    await crypto.subtle.digest('SHA-256', new TextEncoder().encode('Hello world'))
+  );
+  // this is the string "Hello World" for testing, hashed with sha-256 above.
+  const toSign = message;
+  // this requests a signature share from the Lit Node
+  // the signature share will be automatically returned in the HTTP response from the node
+  const sigShare = await LitActions.signEcdsa({ toSign, publicKey: "0x02e5896d70c1bc4b4844458748fe0f936c7919d7968341e391fb6d82c258192e64", sigName: "sig1" });
+};
+
+
+
+go();
+`;
