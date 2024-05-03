7a8f6ed786d411dff4cae2057ab9019e0be4a2f4
Wyatt Barnes <me@wyatt.email>
2024-05-02 18:38:52 -1000

    WIP

---
+++ lit-action-using-fetch/browser/src/index.js
@@ -0,0 +1,97 @@
+import { LitNodeClient } from "@lit-protocol/lit-node-client";
+import { LitAbility, LitActionResource } from "@lit-protocol/auth-helpers";
+import { disconnectWeb3 } from "@lit-protocol/auth-browser";
+import * as ethers from "ethers";
+
+import { litActionCode } from "./litAction";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("myButton").addEventListener("click", buttonClick);
+});
+
+async function buttonClick() {
+  try {
+    console.log("Clicked");
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const ethersSigner = provider.getSigner();
+    console.log("Connected account:", await ethersSigner.getAddress());
+
+    const litNodeClient = await getLitNodeClient();
+
+    const sessionSigs = await getSessionSigs(litNodeClient, ethersSigner);
+    console.log("Got Session Signatures!");
+
+    const message = new Uint8Array(
+      await crypto.subtle.digest(
+        "SHA-256",
+        new TextEncoder().encode("Hello world")
+      )
+    );
+    const litActionSignatures = await litNodeClient.executeJs({
+      code: litActionCode,
+      sessionSigs,
+      jsParams: {
+        toSign: message,
+        publicKey:
+          "0x04f444e7d05b5a7c0eeec713eb66018837e7ea3913da3fc1f1203142a4b67ad48b8adb5ce66045a400f2fc5d6fde6cff1719568dbeeb6466d6782ded45e5fd810a",
+        sigName: "sig",
+      },
+    });
+    console.log("litActionSignatures: ", litActionSignatures);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    disconnectWeb3();
+  }
+}
+
+async function getLitNodeClient() {
+  const litNodeClient = new LitNodeClient({
+    litNetwork: "cayenne",
+  });
+
+  console.log("Connecting litNodeClient to network...");
+  await litNodeClient.connect();
+
+  console.log("litNodeClient connected!");
+  return litNodeClient;
+}
+
+function getAuthNeededCallback(litNodeClient, ethersSigner) {
+  return async ({ resourceAbilityRequests, expiration, uri }) => {
+    console.log("resources", resourceAbilityRequests);
+
+    const toSign = await createSiweMessageWithRecaps({
+      uri,
+      expiration,
+      resources: resourceAbilityRequests,
+      walletAddress: await ethersSigner.getAddress(),
+      nonce: await litNodeClient.getLatestBlockhash(),
+      litNodeClient,
+    });
+
+    const authSig = await generateAuthSig({
+      signer: ethersSigner,
+      toSign,
+    });
+
+    return authSig;
+  };
+}
+
+async function getSessionSigs(litNodeClient, ethersSigner) {
+  console.log("Getting Session Signatures...");
+  return litNodeClient.getSessionSigs({
+    chain: "ethereum",
+    expiration: new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(), // 24 hours
+    resourceAbilityRequests: [
+      {
+        resource: new LitActionResource("*"),
+        ability: LitAbility.LitActionExecution,
+      },
+    ],
+    authNeededCallback: getAuthNeededCallback(litNodeClient, ethersSigner),
+  });
+}
