7814fafb7e42f745a0e74c964ebda57838042e2a
Wyatt Barnes <me@wyatt.email>
2024-05-02 15:52:37 -1000

    Debugging

--- conditional-signing/browser/src/litAction.js
+++ conditional-signing/browser/src/litAction.js
@@ -1,23 +1,19 @@
 export const litActionCode = `
 const go = async () => {
   // test an access control condition
-  const testResult = await Lit.Actions.checkConditions({conditions, sessionSigs, chain})
-
-  console.log('testResult', testResult)
+  // const testResult = await Lit.Actions.checkConditions({conditions, sessionSigs, chain})
 
   // only sign if the access condition is true
-  if (!testResult){
-    return;
-  }
+  // if (!testResult){
+  //   return;
+  // }
 
-  const message = new Uint8Array(
+  const toSign = new Uint8Array(
     await crypto.subtle.digest('SHA-256', new TextEncoder().encode('Hello world'))
   );
-  // this is the string "Hello World" for testing, hashed with sha-256 above.
-  const toSign = message;
   // this requests a signature share from the Lit Node
   // the signature share will be automatically returned in the HTTP response from the node
-  const sigShare = await LitActions.signEcdsa({ toSign, publicKey: "0x02e5896d70c1bc4b4844458748fe0f936c7919d7968341e391fb6d82c258192e64", sigName: "sig1" });
+  const sigShare = await LitActions.signEcdsa({ toSign, publicKey: "0x04f444e7d05b5a7c0eeec713eb66018837e7ea3913da3fc1f1203142a4b67ad48b8adb5ce66045a400f2fc5d6fde6cff1719568dbeeb6466d6782ded45e5fd810a", sigName: "sig1" });
 };
 
 go();
