b9fef5a65e2e1ea609d182325ff6cb2fb9f737fa
Wyatt Barnes <me@wyatt.email>
2024-05-03 15:42:14 -1000

    Init nodejs example

---
+++ eip-191-signing/nodejs/README.md
@@ -0,0 +1,5 @@
+# Running this Example
+
+1. `yarn`
+2. `yarn start`
+3. The PKP signature will be the last output in the console
---
+++ eip-191-signing/nodejs/src/index.js
@@ -0,0 +1,139 @@
+import { LitNodeClientNodeJs } from "@lit-protocol/lit-node-client-nodejs";
+import { LitNetwork } from "@lit-protocol/constants";
+import {
+  createSiweMessageWithRecaps,
+  generateAuthSig,
+  LitAbility,
+  LitActionResource,
+  LitPKPResource,
+} from "@lit-protocol/auth-helpers";
+import { LitContracts } from "@lit-protocol/contracts-sdk";
+import {
+  providers as ethersProviders,
+  utils as ethersUtils,
+  Wallet,
+} from "ethers";
+
+import { litActionCode } from "./litAction.js";
+
+(async () => {
+  let litNodeClient;
+
+  try {
+    const wallet = getWallet();
+    litNodeClient = await getLitNodeClient();
+
+    const sessionSigs = await getSessionSigs(litNodeClient, wallet);
+    console.log("Got Session Signatures!");
+
+    const litActionSignatures = await litNodeClient.executeJs({
+      sessionSigs,
+      code: litActionCode,
+      jsParams: {
+        dataToSign: ethersUtils.arrayify(
+          ethersUtils.keccak256([1, 2, 3, 4, 5])
+        ),
+        publicKey: await getPkpPublicKey(wallet),
+        sigName: "sig",
+      },
+    });
+    console.log("litActionSignatures: ", litActionSignatures);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    litNodeClient.disconnect();
+  }
+})();
+
+function getWallet(privateKey) {
+  if (privateKey !== undefined)
+    return new Wallet(
+      privateKey,
+      new ethersProviders.JsonRpcProvider(
+        "https://chain-rpc.litprotocol.com/http"
+      )
+    );
+
+  if (process.env.PRIVATE_KEY === undefined)
+    throw new Error("Please provide the env: PRIVATE_KEY");
+
+  return new Wallet(
+    process.env.PRIVATE_KEY,
+    new ethersProviders.JsonRpcProvider(
+      "https://chain-rpc.litprotocol.com/http"
+    )
+  );
+}
+
+async function getPkpPublicKey(ethersSigner) {
+  if (
+    process.env.PKP_PUBLIC_KEY !== undefined &&
+    process.env.PKP_PUBLIC_KEY !== ""
+  )
+    return process.env.PKP_PUBLIC_KEY;
+
+  const pkp = await mintPkp(ethersSigner);
+  console.log("Minted PKP!", pkp);
+  return pkp.publicKey;
+}
+
+async function mintPkp(ethersSigner) {
+  console.log("Minting new PKP...");
+  const litContracts = new LitContracts({
+    signer: ethersSigner,
+    network: LitNetwork.Cayenne,
+  });
+
+  await litContracts.connect();
+
+  return (await litContracts.pkpNftContractUtils.write.mint()).pkp;
+}
+
+async function getLitNodeClient() {
+  const litNodeClient = new LitNodeClientNodeJs({
+    litNetwork: LitNetwork.Cayenne,
+  });
+
+  console.log("Connecting litNodeClient to network...");
+  await litNodeClient.connect();
+
+  console.log("litNodeClient connected!");
+  return litNodeClient;
+}
+
+async function getSessionSigs(litNodeClient, ethersSigner) {
+  console.log("Getting Session Signatures...");
+  return litNodeClient.getSessionSigs({
+    chain: "ethereum",
+    expiration: new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(), // 24 hours
+    resourceAbilityRequests: [
+      {
+        resource: new LitPKPResource("*"),
+        ability: LitAbility.PKPSigning,
+      },
+      {
+        resource: new LitActionResource("*"),
+        ability: LitAbility.LitActionExecution,
+      },
+    ],
+    authNeededCallback: getAuthNeededCallback(litNodeClient, ethersSigner),
+  });
+}
+
+function getAuthNeededCallback(litNodeClient, ethersSigner) {
+  return async ({ resourceAbilityRequests, expiration, uri }) => {
+    const toSign = await createSiweMessageWithRecaps({
+      uri,
+      expiration,
+      resources: resourceAbilityRequests,
+      walletAddress: await ethersSigner.getAddress(),
+      nonce: await litNodeClient.getLatestBlockhash(),
+      litNodeClient,
+    });
+
+    return await generateAuthSig({
+      signer: ethersSigner,
+      toSign,
+    });
+  };
+}
