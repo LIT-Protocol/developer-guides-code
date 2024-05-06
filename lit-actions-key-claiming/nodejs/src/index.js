3400785c423225ec18b4e4c15ae9eeb10b0f96c4
Wyatt Barnes <me@wyatt.email>
2024-05-06 13:23:12 -1000

    Init nodejs example

---
+++ lit-actions-key-claiming/nodejs/src/index.js
@@ -0,0 +1,141 @@
+import { LitNodeClientNodeJs } from "@lit-protocol/lit-node-client-nodejs";
+import { LitNetwork } from "@lit-protocol/constants";
+import {
+  createSiweMessageWithRecaps,
+  generateAuthSig,
+  LitAbility,
+  LitActionResource,
+  LitPKPResource,
+} from "@lit-protocol/auth-helpers";
+import { LitContracts } from "@lit-protocol/contracts-sdk";
+import { providers as ethersProviders, Wallet } from "ethers";
+
+import { litActionCode } from "./litAction.js";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PKP_PUBLIC_KEY = process.env.PKP_PUBLIC_KEY;
+
+(async () => {
+  let litNodeClient;
+
+  try {
+    const wallet = getWallet();
+    litNodeClient = await getLitNodeClient();
+
+    const sessionSigs = await getSessionSigs(litNodeClient, wallet);
+    console.log("Got Session Signatures!");
+
+    const result = await litNodeClient.executeJs({
+      sessionSigs,
+      code: litActionCode,
+      jsParams: {
+        userId: "yourUserId",
+      },
+    });
+    console.log("result: ", result);
+
+    const litContractClient = new LitContracts({
+      signer: wallet,
+    });
+
+    const tx = await litContractClient.pkpNftContractUtils.write.claimAndMint(
+      result.claims["yourUserId"].derivedKeyId,
+      result.claims["yourUserId"].signatures
+    );
+    console.log("Claim and Mint Tx:", tx);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    litNodeClient.disconnect();
+  }
+})();
+
+function getWallet(privateKey) {
+  if (privateKey !== undefined)
+    return new Wallet(
+      privateKey,
+      new ethersProviders.JsonRpcProvider(
+        "https://chain-rpc.litprotocol.com/http"
+      )
+    );
+
+  if (PRIVATE_KEY === undefined)
+    throw new Error("Please provide the env: PRIVATE_KEY");
+
+  return new Wallet(
+    PRIVATE_KEY,
+    new ethersProviders.JsonRpcProvider(
+      "https://chain-rpc.litprotocol.com/http"
+    )
+  );
+}
+
+async function getPkpPublicKey(ethersSigner) {
+  if (PKP_PUBLIC_KEY !== undefined && PKP_PUBLIC_KEY !== "")
+    return PKP_PUBLIC_KEY;
+
+  const pkp = await mintPkp(ethersSigner);
+  console.log("Minted PKP!", pkp);
+  return pkp.publicKey;
+}
+
+async function mintPkp(ethersSigner) {
+  console.log("Minting new PKP...");
+  const litContracts = new LitContracts({
+    signer: ethersSigner,
+    network: LitNetwork.Cayenne,
+  });
+
+  await litContracts.connect();
+
+  return (await litContracts.pkpNftContractUtils.write.mint()).pkp;
+}
+
+async function getLitNodeClient() {
+  const litNodeClient = new LitNodeClientNodeJs({
+    litNetwork: LitNetwork.Cayenne,
+  });
+
+  console.log("Connecting litNodeClient to network...");
+  await litNodeClient.connect();
+
+  console.log("litNodeClient connected!");
+  return litNodeClient;
+}
+
+async function getSessionSigs(litNodeClient, ethersSigner) {
+  console.log("Getting Session Signatures...");
+  return litNodeClient.getSessionSigs({
+    chain: "ethereum",
+    expiration: new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(), // 24 hours
+    resourceAbilityRequests: [
+      {
+        resource: new LitPKPResource("*"),
+        ability: LitAbility.PKPSigning,
+      },
+      {
+        resource: new LitActionResource("*"),
+        ability: LitAbility.LitActionExecution,
+      },
+    ],
+    authNeededCallback: getAuthNeededCallback(litNodeClient, ethersSigner),
+  });
+}
+
+function getAuthNeededCallback(litNodeClient, ethersSigner) {
+  return async ({ resourceAbilityRequests, expiration, uri }) => {
+    const toSign = await createSiweMessageWithRecaps({
+      uri,
+      expiration,
+      resources: resourceAbilityRequests,
+      walletAddress: await ethersSigner.getAddress(),
+      nonce: await litNodeClient.getLatestBlockhash(),
+      litNodeClient,
+    });
+
+    return await generateAuthSig({
+      signer: ethersSigner,
+      toSign,
+    });
+  };
+}
--- lit-actions-key-claiming/src/litAction.js
+++
@@ -1,3 +0,0 @@
-export const litActionCode = `(async () => {
-  Lit.Actions.claimKey({keyId: userId});
-})();`;
