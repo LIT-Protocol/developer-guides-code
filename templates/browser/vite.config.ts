a268fcfae2a64757bff25becea74cdf659d7c3fc
awisniew207 <andrew.wisniewski.08@gmail.com>
2024-07-24 10:20:40 -0700

    Same updates as the quickstart, but done to the templates

--- templates/browser/vite.config.ts
+++ templates/browser/vite.config.ts
@@ -1,26 +1,44 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['buffer', 'process', 'util']
+    }),
+  ],
   define: {
-    global: "window",
+    'process.env': {},
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      // Remove the buffer alias
+    },
   },
   optimizeDeps: {
     esbuildOptions: {
-      // Node.js global to browser globalThis
       define: {
-        global: "globalThis",
+        global: 'globalThis',
       },
-      // Enable esbuild polyfill plugins
       plugins: [
         NodeGlobalsPolyfillPlugin({
-          process: true,
           buffer: true,
+          process: true
         }),
         NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill(),
       ],
     },
   },
