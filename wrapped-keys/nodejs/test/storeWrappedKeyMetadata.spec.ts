709ade3e274d48dd74e4924d3475e304fc327182
Wyatt Barnes <me@wyatt.email>
2024-07-24 11:21:23 -1000

    Update store metadata code and example

--- wrapped-keys/nodejs/test/storeWrappedKeyMetadata.spec.ts
+++ wrapped-keys/nodejs/test/storeWrappedKeyMetadata.spec.ts
@@ -1,5 +1,7 @@
 import { expect } from "chai";
 import * as ethers from "ethers";
+import { LIT_RPC } from "@lit-protocol/constants";
+import { Keypair } from "@solana/web3.js";
 
 import { getEnv, mintPkp } from "../src/utils";
 import { storeWrappedKeyMetadata } from "../src/storeWrappedKeyMetadata";
@@ -15,9 +17,7 @@ describe("Importing an Ethereum key using importPrivateKey", () => {
     this.timeout(120_000);
     ethersSigner = new ethers.Wallet(
       ETHEREUM_PRIVATE_KEY,
-      new ethers.providers.JsonRpcProvider(
-        "https://chain-rpc.litprotocol.com/http"
-      )
+      new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE)
     );
 
     mintedPkp = await mintPkp(ethersSigner);
@@ -30,6 +30,36 @@ describe("Importing an Ethereum key using importPrivateKey", () => {
       NEW_ETHEREUM_KEYPAIR_WALLET.privateKey,
       NEW_ETHEREUM_KEYPAIR_WALLET.publicKey,
       "K256"
+    );
+
+    expect(success).true;
+  }).timeout(120_000);
+});
+
+describe("Importing a Solana key using importPrivateKey", () => {
+  let ethersSigner: ethers.Wallet;
+  let solanaKeypair: Keypair;
+  let mintedPkp;
+
+  before(async function () {
+    this.timeout(120_000);
+    ethersSigner = new ethers.Wallet(
+      ETHEREUM_PRIVATE_KEY,
+      new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE)
+    );
+
+    solanaKeypair = Keypair.generate();
+
+    mintedPkp = await mintPkp(ethersSigner);
+  });
+
+  it("should import a Solana key and attach it to a new PKP", async () => {
+    const success = await storeWrappedKeyMetadata(
+      mintedPkp!.publicKey,
+      mintedPkp!.ethAddress,
+      Buffer.from(solanaKeypair.secretKey).toString("hex"),
+      solanaKeypair.publicKey.toString(),
+      "ed25519"
     );
 
     expect(success).true;
