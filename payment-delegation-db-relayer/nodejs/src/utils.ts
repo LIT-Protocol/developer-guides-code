22e65a1925f8414ef72d5d89915e21a180b2515d
Wyatt Barnes <me@wyatt.email>
2024-07-31 11:22:23 -1000

    WIP test example

--- payment-delegation-db-relayer/nodejs/src/utils.ts
+++ payment-delegation-db-relayer/nodejs/src/utils.ts
@@ -1,3 +1,7 @@
+import * as ethers from "ethers";
+import { LitContracts } from "@lit-protocol/contracts-sdk";
+import { LitNetwork } from "@lit-protocol/constants";
+
 export const getEnv = (name: string): string => {
   const env = process.env[name];
   if (env === undefined || env === "")
@@ -5,4 +9,25 @@ export const getEnv = (name: string): string => {
       `${name} ENV is not defined, please define it in the .env file`
     );
   return env;
+};
+
+export const mintPkp = async (ethersSigner: ethers.Wallet) => {
+  try {
+    console.log("🔄 Connecting LitContracts client to network...");
+    const litContracts = new LitContracts({
+      signer: ethersSigner,
+      network: LitNetwork.DatilDev,
+    });
+    await litContracts.connect();
+    console.log("✅ Connected LitContracts client to network");
+
+    console.log("🔄 Minting new PKP...");
+    const pkp = (await litContracts.pkpNftContractUtils.write.mint()).pkp;
+    console.log(
+      `✅ Minted new PKP with public key: ${pkp.publicKey} and ETH address: ${pkp.ethAddress}`
+    );
+    return pkp;
+  } catch (error) {
+    console.error(error);
+  }
 };
---
+++ payment-delegation-db-relayer/nodejs/test/generateWrappedKey.spec.ts
@@ -0,0 +1,107 @@
+import { expect } from "chai";
+import ethers from "ethers";
+import { LIT_RPC, LitNetwork } from "@lit-protocol/constants";
+import { LitNodeClient } from "@lit-protocol/lit-node-client";
+
+import { registerPayer } from "../src/registerPayer";
+import { addUsers } from "../src/addUsers";
+import { getEnv, mintPkp } from "../src/utils";
+import {
+  EthWalletProvider,
+  LitAuthClient,
+} from "@lit-protocol/lit-auth-client";
+import {
+  LitAbility,
+  LitActionResource,
+  LitPKPResource,
+} from "@lit-protocol/auth-helpers";
+
+const ETHEREUM_PRIVATE_KEY = getEnv("ETHEREUM_PRIVATE_KEY");
+const LIT_RELAYER_API_KEY = getEnv("LIT_RELAYER_API_KEY");
+
+describe("Generate a Wrapped Key using a payee PKP", () => {
+  const payeeEthersSigner = ethers.Wallet.createRandom();
+  const ethersSigner = new ethers.Wallet(
+    ETHEREUM_PRIVATE_KEY,
+    new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE)
+  );
+
+  it("should generate a Wrapped Key", async () => {
+    let litNodeClient: LitNodeClient;
+
+    try {
+      console.log("🔄 Connecting to Lit network...");
+      litNodeClient = new LitNodeClient({
+        litNetwork: LitNetwork.DatilTest,
+        debug: true,
+      });
+      await litNodeClient.connect();
+      console.log("✅ Connected to Lit network");
+
+      console.log(
+        "🔄 Initializing LitAuthClient for authentication through Lit login..."
+      );
+      const litAuthClient = new LitAuthClient({
+        debug: false,
+        litRelayConfig: {
+          relayApiKey: LIT_RELAYER_API_KEY,
+        },
+        rpcUrl: LIT_RPC.CHRONICLE_YELLOWSTONE,
+        litNodeClient,
+      });
+      console.log("✅ Successfully initialized LitAuthClient instance");
+
+      console.log(
+        "🔄 Authenticating EthWallerProvider, generating an Auth Method..."
+      );
+      const authMethod = await EthWalletProvider.authenticate({
+        signer: payeeEthersSigner,
+        litNodeClient,
+      });
+      console.log(
+        "✅ Successfully authenticated EthWallerProvider, generated Auth Method"
+      );
+
+      console.log("🔄 Minting a PKP using Auth Method...");
+      const payeePkpInfo = await litAuthClient.mintPKPWithAuthMethods(
+        [authMethod],
+        {}
+      );
+      console.log("✅ Successfully minted a PKP using the Auth Method");
+
+      console.log("🔄 Getting the Session Sigs for the PKP...");
+      const sessionSigs = await litNodeClient.getPkpSessionSigs({
+        pkpPublicKey: payeePkpInfo.pkpPublicKey!,
+        authMethods: [authMethod],
+        resourceAbilityRequests: [
+          {
+            resource: new LitPKPResource("*"),
+            ability: LitAbility.PKPSigning,
+          },
+          {
+            resource: new LitActionResource("*"),
+            ability: LitAbility.LitActionExecution,
+          },
+        ],
+        expiration: new Date(Date.now() + 1000 * 60 * 10).toISOString(), // 10 minutes
+      });
+      console.log("✅ Got PKP Session Sigs");
+
+      const newPayerInfo = await registerPayer();
+      await addUsers([payeePkpInfo.pkpEthAddress as string]);
+
+      console.log("🔄 Executing simple Lit Action...");
+      const result = await litNodeClient.executeJs({
+        sessionSigs,
+        code: `(() => console.log("The answer to the universe is 42."))();`,
+      });
+      console.log("✅ Executed simple Lit Action");
+
+      console.log(result);
+    } catch (error) {
+      throw error;
+    } finally {
+      litNodeClient!.disconnect();
+    }
+  }).timeout(60_000);
+});
