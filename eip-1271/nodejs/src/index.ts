254ac1163a2c037590b59f3a3351deb7d05c203a
Wyatt Barnes <me@wyatt.email>
2024-08-21 22:09:37 -1000

    Move getPkpInfoFromMintReceipt to utils file

--- eip-1271/nodejs/src/index.ts
+++ eip-1271/nodejs/src/index.ts
@@ -12,7 +12,7 @@ import { LitAbility, LitPKPResource } from "@lit-protocol/auth-helpers";
 // @ts-ignore
 import IpfsHash from "ipfs-only-hash";
 
-import { getEnv } from "./utils";
+import { getEnv, getPkpInfoFromMintReceipt } from "./utils";
 import { litActionCode } from "./litAction";
 import WhitelistEIP1271 from "./WhitelistEIP1271.json";
 
@@ -221,27 +221,4 @@ export const runExample = async () => {
   } finally {
     litNodeClient!.disconnect();
   }
-};
-
-const getPkpInfoFromMintReceipt = async (
-  txReceipt: ethers.ContractReceipt,
-  litContractsClient: LitContracts
-) => {
-  const pkpMintedEvent = txReceipt!.events!.find(
-    (event) =>
-      event.topics[0] ===
-      "0x3b2cc0657d0387a736293d66389f78e4c8025e413c7a1ee67b7707d4418c46b8"
-  );
-
-  const publicKey = "0x" + pkpMintedEvent!.data.slice(130, 260);
-  const tokenId = ethers.utils.keccak256(publicKey);
-  const ethAddress = await litContractsClient.pkpNftContract.read.getEthAddress(
-    tokenId
-  );
-
-  return {
-    tokenId: ethers.BigNumber.from(tokenId).toString(),
-    publicKey,
-    ethAddress,
-  };
 };
--- eip-1271/nodejs/src/utils.ts
+++ eip-1271/nodejs/src/utils.ts
@@ -1,3 +1,6 @@
+import { ethers } from "ethers";
+import { LitContracts } from "@lit-protocol/contracts-sdk";
+
 export const getEnv = (name: string): string => {
   const env = process.env[name];
   if (env === undefined || env === "")
@@ -5,4 +8,27 @@ export const getEnv = (name: string): string => {
       `${name} ENV is not defined, please define it in the .env file`
     );
   return env;
+};
+
+export const getPkpInfoFromMintReceipt = async (
+  txReceipt: ethers.ContractReceipt,
+  litContractsClient: LitContracts
+) => {
+  const pkpMintedEvent = txReceipt!.events!.find(
+    (event) =>
+      event.topics[0] ===
+      "0x3b2cc0657d0387a736293d66389f78e4c8025e413c7a1ee67b7707d4418c46b8"
+  );
+
+  const publicKey = "0x" + pkpMintedEvent!.data.slice(130, 260);
+  const tokenId = ethers.utils.keccak256(publicKey);
+  const ethAddress = await litContractsClient.pkpNftContract.read.getEthAddress(
+    tokenId
+  );
+
+  return {
+    tokenId: ethers.BigNumber.from(tokenId).toString(),
+    publicKey,
+    ethAddress,
+  };
 };
