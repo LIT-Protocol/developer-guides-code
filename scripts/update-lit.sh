bce6dbd4af2bf41608d53799a42ab7b3a1075485
Ansh <anshsaxena4190@gmail.com>
2024-09-08 10:23:59 +0530

    fix: scripts bug fix

--- scripts/update-lit.sh
+++ scripts/update-lit.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 TARGET_DIR="${1:-$(pwd)}"
+TARGET_DIR=$(realpath "$TARGET_DIR")
 
-if [ -f "$TARGET_DIR/package.json" ]; then
+if [ -d "$TARGET_DIR" ]; then
+  cd "$TARGET_DIR" || exit
   echo "Updating lit-protocol dependencies in $TARGET_DIR"
   
-  for pkg in $(jq -r '.dependencies | keys[] | select(test("^@lit-protocol"))' "$TARGET_DIR/package.json"); do
-    LATEST_VERSION=$(npm show $pkg version)
-    echo "Updating $pkg to version ^$LATEST_VERSION"
-    npm pkg set "dependencies.$pkg=^$LATEST_VERSION"
-  done
+  if [ -f "package.json" ]; then
+    for pkg in $(jq -r '.dependencies | keys[] | select(test("^@lit-protocol"))' "package.json"); do
+      LATEST_VERSION=$(npm show $pkg version)
+      echo "Updating $pkg to version ^$LATEST_VERSION"
+      npm pkg set "dependencies.$pkg=^$LATEST_VERSION"
+    done
+  else
+    echo "No package.json found in $TARGET_DIR."
+  fi
 else
-  echo "No package.json found in $TARGET_DIR."
+  echo "Directory $TARGET_DIR does not exist."
 fi
