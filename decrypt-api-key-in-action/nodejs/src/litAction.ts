44d5567de7129426ce2bbc9a2658d0ff13e07836
Wyatt Barnes <me@wyatt.email>
2024-09-13 14:41:06 -1000

    WIP debug LA

--- decrypt-api-key-in-action/nodejs/src/litAction.ts
+++ decrypt-api-key-in-action/nodejs/src/litAction.ts
@@ -10,26 +10,34 @@ const _litActionCode = async () => {
       chain: "ethereum",
     });
 
-    const resp = await fetch(`${alchemyUrl}${apiKey}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        id: 1,
-        method: "eth_blockNumber",
-        params: [],
-      }),
-    });
+    const jsonRpcResponse = await Lit.Actions.runOnce(
+      { waitForResponse: true, name: "ETH block number" },
+      async () => {
+        const resp = await fetch(`${alchemyUrl}${apiKey}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            jsonrpc: "2.0",
+            id: 1,
+            method: "eth_blockNumber",
+            params: [],
+          }),
+        });
 
-    let data = await resp.json();
+        let data = await resp.json();
 
-    if (data.result) {
-      data.result = parseInt(data.result, 16);
-    }
+        if (data.result) {
+          data.result = parseInt(data.result, 16);
+          return data.result;
+        } else {
+          throw new Error("Failed to get block number");
+        }
+      }
+    );
 
-    Lit.Actions.setResponse({ response: JSON.stringify(data) });
+    Lit.Actions.setResponse({ response: JSON.stringify(jsonRpcResponse) });
   } catch (e) {
     Lit.Actions.setResponse({ response: e.message });
   }
