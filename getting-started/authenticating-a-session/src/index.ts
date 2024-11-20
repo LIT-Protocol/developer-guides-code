1b8f7c170984214e61249ccc7bc661c778bc5122
Wyatt Barnes <me@wyatt.email>
2024-11-20 17:36:25 +0900

    Init getting started examples

---
+++ getting-started/authenticating-a-session/src/index.ts
@@ -0,0 +1,65 @@
+import { LitNodeClient } from '@lit-protocol/lit-node-client';
+import { LIT_ABILITY, LIT_NETWORK, LIT_RPC } from '@lit-protocol/constants';
+import {
+  createSiweMessage,
+  generateAuthSig,
+  LitAccessControlConditionResource,
+} from '@lit-protocol/auth-helpers';
+import { ethers } from 'ethers';
+
+import { getEnv } from './utils';
+
+const ETHEREUM_PRIVATE_KEY = getEnv('ETHEREUM_PRIVATE_KEY');
+
+export const runExample = async () => {
+  let litNodeClient: LitNodeClient;
+
+  try {
+    const ethersSigner = new ethers.Wallet(
+      ETHEREUM_PRIVATE_KEY,
+      new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE)
+    );
+
+    litNodeClient = new LitNodeClient({
+      litNetwork: LIT_NETWORK.DatilDev,
+      debug: true,
+    });
+    await litNodeClient.connect();
+
+    const sessionSignatures = await litNodeClient.getSessionSigs({
+      chain: 'ethereum',
+      expiration: new Date(Date.now() + 1000 * 60 * 10).toISOString(), // 10 minutes
+      resourceAbilityRequests: [
+        {
+          resource: new LitAccessControlConditionResource('*'),
+          ability: LIT_ABILITY.AccessControlConditionDecryption,
+        },
+      ],
+      authNeededCallback: async ({
+        uri,
+        expiration,
+        resourceAbilityRequests,
+      }) => {
+        const toSign = await createSiweMessage({
+          uri,
+          expiration,
+          resources: resourceAbilityRequests,
+          walletAddress: await ethersSigner.getAddress(),
+          nonce: await litNodeClient.getLatestBlockhash(),
+          litNodeClient,
+        });
+
+        return await generateAuthSig({
+          signer: ethersSigner,
+          toSign,
+        });
+      },
+    });
+
+    return sessionSignatures;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    litNodeClient!.disconnect();
+  }
+};
---
+++ getting-started/connecting-to-lit/src/index.ts
@@ -0,0 +1,18 @@
+import { LitNodeClient } from '@lit-protocol/lit-node-client';
+import { LitNetwork } from '@lit-protocol/constants';
+
+export const runExample = async () => {
+  let litNodeClient: LitNodeClient;
+
+  try {
+    litNodeClient = new LitNodeClient({
+      litNetwork: LitNetwork.DatilDev,
+      debug: true,
+    });
+    await litNodeClient.connect();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    litNodeClient!.disconnect();
+  }
+};
