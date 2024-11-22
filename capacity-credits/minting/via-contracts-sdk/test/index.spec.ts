ba81f547f6c90eace57e586044b75423026e6771
Wyatt Barnes <me@wyatt.email>
2024-11-22 23:29:22 +0900

    Init mint credit via sdk example

---
+++ capacity-credits/minting/via-contracts-sdk/test/index.spec.ts
@@ -0,0 +1,47 @@
+import { expect, use } from 'chai';
+import chaiJsonSchema from 'chai-json-schema';
+
+import { runExample } from '../src/index';
+
+use(chaiJsonSchema);
+
+describe('Testing minting a Capacity Credit', () => {
+  before(async function () {
+    this.timeout(120_000);
+  });
+
+  it('should mint a new capacity credit', async () => {
+    const expectedCapacityCreditInfoSchema = {
+      type: 'object',
+      required: ['rliTxHash', 'capacityTokenId', 'capacityTokenIdStr'],
+      properties: {
+        rliTxHash: {
+          type: 'string',
+          pattern: '^0x[a-fA-F0-9]{64}$',
+        },
+        capacityTokenId: {
+          type: 'object',
+          properties: {
+            hex: {
+              type: 'string',
+              pattern: '^0x[a-fA-F0-9]+$',
+            },
+            isBigNumber: {
+              type: 'boolean',
+              enum: [true],
+            },
+          },
+        },
+        capacityTokenIdStr: {
+          type: 'string',
+          pattern: '^[0-9]+$',
+        },
+      },
+    };
+
+    const capacityCreditInfo = await runExample();
+    expect(capacityCreditInfo).to.be.jsonSchema(
+      expectedCapacityCreditInfoSchema
+    );
+  }).timeout(120_000);
+});
