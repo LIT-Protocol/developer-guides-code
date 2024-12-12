06b0288a13f8499771706a3639e01735de2a14d6
Ansh <anshsaxena4190@gmail.com>
2024-12-13 03:31:03 +0530

    migrating packages

--- hacker-guides/encryption/encrypt-large-file/src/index.ts
+++ hacker-guides/encryption/encrypt-large-file/src/index.ts
@@ -2,9 +2,9 @@ import { type LitNodeClient } from "@lit-protocol/lit-node-client";
 import {
   createSiweMessage,
   generateAuthSig,
-  LitAbility,
   LitAccessControlConditionResource,
 } from "@lit-protocol/auth-helpers";
+import { LIT_ABILITY } from "@lit-protocol/constants";
 import { AccessControlConditions } from "@lit-protocol/types";
 import { promises as fs } from "fs";
 import { join } from "path";
@@ -139,7 +139,7 @@ export const encryptLargeFile = async () => {
       resourceAbilityRequests: [
         {
           resource: new LitAccessControlConditionResource("*"),
-          ability: LitAbility.AccessControlConditionDecryption,
+          ability: LIT_ABILITY.AccessControlConditionDecryption,
         },
       ],
       authNeededCallback: async ({
